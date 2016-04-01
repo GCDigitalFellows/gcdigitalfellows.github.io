aec7fd6624207b7facdd8b7a8cf2e8056e64cde3
Keith Miyake <keith.miyake@gmail.com>
2016-03-31 20:34:30 -0400

    lots of gulpfile fixes

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,15 @@ var gulp = require('gulp');
 var gulpLoadPlugins = require('gulp-load-plugins');
 var $ = gulpLoadPlugins();
 var spawn = require('child_process').spawn;
-var browserSync = require('browser-sync');
-var eslint = require('eslint');
 var argv = require('yargs').argv;
 var autoprefixer = require('autoprefixer');
 var mqpacker = require('css-mqpacker');
 var csswring = require('csswring');
 var del = require('del');
-var imagemin = require('gulp-imagemin');
-
+var browserSync = require('browser-sync');
+var commandExists = require('command-exists');
 
 var isProduction = argv.nomin;
-
 var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 
 gulp.task('clean:assets', function () {
@@ -52,7 +49,7 @@ gulp.task('fonts', function () {
 
 gulp.task('images', function () {
   return gulp.src(['_images/*'])
-    .pipe(imagemin({
+    .pipe($.imagemin({
       progressive: true,
       svgoPlugins: [
         {removeViewBox: false},
@@ -64,21 +61,67 @@ gulp.task('images', function () {
 });
 
 gulp.task('jekyll', function (done) {
-  spawn('bundle', ['exec', jekyll, 'build'], {
-    stdio: 'inherit'
-  })
-  .on('exit', function (code) {
-    done(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
+  commandExists('bundle', function (err, commandExists) {
+    if (err !== null) {
+      console.log(err);
+    } else if (commandExists) {
+      console.log('Starting Jekyll using Bundler');
+      spawn('bundle', ['exec', jekyll, 'build'], {stdio: 'inherit'})
+      .on('exit', function (code) {
+        done(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
+      });
+    } else {
+      console.log('Starting Jekyll without Bundler');
+      spawn(jekyll, ['build'], {stdio: 'inherit'})
+      .on('exit', function (code) {
+        done(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
+      });
+    }
   });
 });
 
 gulp.task('jekyll:incremental', function (done) {
-  spawn('bundle', ['exec', jekyll, 'build', '--incremental'], {
-    stdio: 'inherit'
-  })
-  .on('exit', function (code) {
-    done(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
+  commandExists('bundle', function (err, commandExists) {
+    if (err !== null) {
+      console.log(err);
+    } else if (commandExists) {
+      console.log('Compiling Jekyll incrementally using Bundler');
+      spawn('bundle', ['exec', jekyll, 'build', '--incremental'], {stdio: 'inherit'})
+      .on('exit', function (code) {
+        done(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
+      });
+    } else {
+      console.log('Compiling Jekyll incrementally without Bundler');
+      spawn(jekyll, ['build', '--incremental'], {stdio: 'inherit'})
+      .on('exit', function (code) {
+        done(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
+      });
+    }
   });
+});
+
+// 'gulp lint' -- check your JS for formatting errors using XO Space
+gulp.task('lint', () =>
+  gulp.src([
+    'gulpfile.babel.js',
+    'js/**/*.js',
+    '!js/**/*.min.js'
+  ])
+  .pipe($.eslint())
+  .pipe($.eslint.formatEach())
+  .pipe($.eslint.failOnError())
+);
+
+gulp.task('scripts', function () {
+  return gulp.src('js/main.js')
+    .pipe($.if(!isProduction, $.sourcemaps.init()))
+    .pipe($.concat('main.min.js'))
+    .pipe($.if(isProduction, $.uglify({preserveComments: 'some'})))
+    .pipe($.if(!isProduction, $.sourcemaps.write('.')))
+    .pipe($.if(isProduction, $.gzip({append: true})))
+    .pipe(gulp.dest('js/'))
+    .pipe(gulp.dest('_site/js'))
+    .pipe($.if(!isProduction, browserSync.stream()));
 });
 
 gulp.task('scripts:vendor', function () {
@@ -101,18 +144,6 @@ gulp.task('scripts:vendor', function () {
     .pipe($.if(isProduction, gulp.dest('js')))
     .pipe($.if(isProduction, $.if('*.js', $.gzip({append: true}))))
     .pipe(gulp.dest('js'))
-    .pipe(gulp.dest('_site/js'))
-    .pipe($.if(!isProduction, browserSync.stream()));
-});
-
-gulp.task('scripts', function () {
-  return gulp.src('js/main.js')
-    .pipe($.if(!isProduction, $.sourcemaps.init()))
-    .pipe($.concat('main.min.js'))
-    .pipe($.if(isProduction, $.uglify({preserveComments: 'some'})))
-    .pipe($.if(!isProduction, $.sourcemaps.write('.')))
-    .pipe($.if(isProduction, $.gzip({append: true})))
-    .pipe(gulp.dest('js/'))
     .pipe(gulp.dest('_site/js'))
     .pipe($.if(!isProduction, browserSync.stream()));
 });
@@ -157,42 +188,6 @@ gulp.task('styles:vendor', function () {
   .pipe(gulp.dest('_site/css'))
   .pipe($.if(!isProduction, browserSync.stream()));
 });
-gulp.task('serve', function (done) {
-  browserSync({
-    server: {
-      baseDir: ['_site']
-    }
-  });
-
-  gulp.watch([
-    './**/*.md',
-    './**/*.markdown',
-    './**/*.html',
-    './**/*.yml',
-    './**/*.json',
-    './**/*.txt',
-    '!_site/*'
-  ], [
-    'jekyll:incremental',
-    browserSync.reload
-  ]);
-  gulp.watch(['js/**/*.js'], ['scripts', browserSync.reload]);
-  gulp.watch(['_sass/**/*.{scss,sass}'], ['styles', browserSync.reload]);
-  gulp.watch(['_images/**/*'], ['images', browserSync.reload]);
-  done();
-});
-
-// 'gulp lint' -- check your JS for formatting errors using XO Space
-gulp.task('lint', () =>
-gulp.src([
-  'gulpfile.babel.js',
-  'js/**/*.js',
-  '!js/**/*.min.js'
-])
-.pipe(eslint())
-.pipe(eslint.formatEach())
-.pipe(eslint.failOnError())
-);
 
 // 'gulp assets' -- cleans out your assets and rebuilds them
 // 'gulp assets --prod' -- cleans out your assets and rebuilds them with
@@ -211,33 +206,54 @@ gulp.task('assets', [
 // 'gulp build' -- same as 'gulp' but doesn't serve your site in your browser
 // 'gulp build --prod' -- same as above but with production settings
 gulp.task('build', [
-  'clean:assets',
   'assets',
   'jekyll'
 ]);
 
-// // 'gulp deploy:push' -- pushes your dist folder to Github
-// gulp.task('deploy:push', () => {
-//   return gulp.src('dist#<{(||)}>#*')
-//   .pipe($.ghPages({
-//     branch: 'master',
-//     remoteUrl: 'git@github.com:GCDigitalFellows/gcdigitalfellows.github.io.git'
-//   }));
-// });
-//
-// // 'gulp deploy' -- copies CNAME and pushes to github
-// gulp.task('deploy', gulp.start(
-//   'build',
-//   // 'deploy:cname',
-//   'deploy:push'
-// ));
+var browserSyncConfig = {
+  server: {
+    baseDir: ['_site']
+  },
+  open: true,
+  browser: [
+    // 'safari',
+    // 'firefox',
+    'chrome'
+  ]
+};
+
+gulp.task('serve', ['build'], () => {
+  browserSync(browserSyncConfig);
+
+  // Jekyll
+  gulp.watch([
+    '_config.yml',
+    '**/*.md',
+    '**/*.markdown',
+    '**/*.html',
+    '_data/yml',
+    '_data/*.json',
+    '!_site/',
+    '!_js/',
+    '!_css/',
+    '!_scss/',
+    '!images/',
+    '!_images/',
+    '!bower_components/',
+    '!node_modules/'
+  ], {interval: 500}, [
+    'jekyll:incremental',
+    browserSync.reload
+  ]);
+
+  // Assets
+  gulp.watch(['js/**/*.js'], {interval: 500}, ['scripts', browserSync.reload]);
+  gulp.watch(['_sass/**/*.{scss,sass}'], {interval: 500}, ['styles', browserSync.reload]);
+  gulp.watch(['_images/**/*'], {interval: 500}, ['images', browserSync.reload]);
+});
 
 // 'gulp' -- cleans your assets and gzipped files, creates your assets and
 // injects them into the templates, then builds your site, copied the assets
 // into their directory and serves the site
-// 'gulp --prod' -- same as above but with production settings
-gulp.task('default', [
-  'build',
-  'serve'
-]);
+gulp.task('default', ['serve']);
 
